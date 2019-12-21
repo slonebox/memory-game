ddec1ef2da3622ce0339b5d8465d4da646289ade
William Mooney Sloneker <wmsloneker@gmail.com>
2019-12-21 12:52:32 -0500

    Added Bootstrap to Index

--- memory-game/src/components/Jumbotron/index.js
+++ memory-game/src/components/Jumbotron/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Jumbotron () {
     return (
         <div className="jumbotron">
-            <h1>Forgetful Fiona!</h1>
+            <h1 className="justify-content-center">Forgetful Fiona!</h1>
             <p>Click on the different photos of Fiona to score points. If you click on the same Fiona twice, your streak ends and your score resets to zero. Good luck!</p>
         </div>
     )
