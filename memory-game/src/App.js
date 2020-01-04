268f8a5d1651fda7c52b540a42f37cabe39b3eec
William Mooney Sloneker <wmsloneker@gmail.com>
2020-01-04 12:48:26 -0500

    Created framework for first functions handling onClick events

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -9,6 +9,28 @@ class App extends Component {
     fionas
   };
 
+  // check if inside past selection
+  handleCardClick = (e) => {
+    e.preventDefault();
+    console.log("Fiona was clicked");
+    // call correct / incorrect
+  }
+
+  correct = () =>{
+    // score up
+    // msg change
+    // call shuffle
+  }
+
+  incorrect = () =>{
+    // msg change
+    // call shuffle
+  }
+  shuffle = () => {
+    // rearage cards
+    // set to state
+  }
+
   //Displays components on the page
   render() {
     return (
@@ -16,6 +38,7 @@ class App extends Component {
       <Jumbotron/>
       {this.state.fionas.map(fiona =>( 
       <MemoryCard
+        handleCardClick={this.handleCardClick}
         id={fiona.id}
         image={fiona.image}
         alt={fiona.alt}
--- memory-game/src/components/MemoryCard/index.js
+++ memory-game/src/components/MemoryCard/index.js
@@ -5,7 +5,7 @@ function MemoryCard(props) {
     return (
         <div className="card justify-content-center">
             <div className="img-container">
-                <img alt={props.alt} src={props.image}/>
+                <img alt={props.alt} src={props.image} dataID={props.id} onClick={props.handleCardClick}/>
             </div>
         </div>
     )
